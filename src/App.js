d06aed9c700c4d6ff1ce8d55af36cdfd0aac1de1
SO HUI <haejuuun925@gmail.com>
2023-09-13 21:45:26 +0900

    feat: ResultTable 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
+import UserInput from './components/UserInput/UserInput';
+import ResultTable from './components/ResultTable/ResultTable';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <UserInput />
+      <ResultTable />
     </div>
   );
 }
